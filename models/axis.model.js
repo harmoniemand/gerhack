6fa70fd3150e8be88fc9050837b8bbd24ccb9c73
Armin Hueneburg <hueneburg.armin@gmail.com>
2016-09-22 19:17:09 +0200

    added model for robots

---
+++ models/axis.model.js
@@ -0,0 +1,15 @@
+module.exports = {
+    Name: "string",
+    RobInMove: "boolean",
+    Runtime: "int",
+    Timestamp: "int",
+    AxisAct: {
+    	// Speed in Degrees/Second. Negative for reverse.
+        A1: "double",
+        A2: "double",
+        A3: "double",
+        A4: "double",
+        A5: "double",
+        A6: "double"
+    }
+}
